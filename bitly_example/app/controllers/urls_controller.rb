c794a6ad2400240b7cf2764f70b45ab7e7098dc6
codeamac01 <codeamac01@gmail.com>
2016-09-20 14:33:47 -0500

    Ya se crean los objetos con todo y su short_url

--- bitly_example/app/controllers/urls_controller.rb
+++ bitly_example/app/controllers/urls_controller.rb
@@ -1,13 +1,28 @@
 class UrlsController < ApplicationController
+  #GET /urls
   def index
     @urls = Url.all
   end
 
+  #GET /urls/:id
   def show
     @url = Url.find(params[:id])
   end
 
+  #GET /urls/new
   def new
     @url = Url.new
   end
-end
+
+  #POST /urls
+  def create
+    # p"*"*50
+    # p params
+    # p params[:url][:long_url]
+    @url = Url.new(long_url: params[:url][:long_url])
+    if @url.save
+      redirect_to @url
+    end
+  end
+
+end#UrlsController
