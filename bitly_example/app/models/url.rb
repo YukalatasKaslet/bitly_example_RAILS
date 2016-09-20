94ac274ac909cac4e9f1e00057f1d3b2abffd6f6
codeamac01 <codeamac01@gmail.com>
2016-09-20 14:33:07 -0500

    añadí validaciones y el before_create

--- bitly_example/app/models/url.rb
+++ bitly_example/app/models/url.rb
@@ -1,2 +1,14 @@
 class Url < ActiveRecord::Base
+  #antes de crear manda a llamar al método mencionado
+  before_create :create_short_url
+
+  #validaciones correctas :D
+  validates :long_url, presence: true, uniqueness: true, format: { with: URI.regexp }
+  
+  private
+  def create_short_url
+    #aquí se crea la url corta, tiene self por que es un metodo de instancia y antes de crearlo se le asigna un valor a su atributo.
+    self.short_url = "Yuk-" + (1..6).map{ ('a'..'z').to_a[rand(26)]}.join
+
+  end
 end
