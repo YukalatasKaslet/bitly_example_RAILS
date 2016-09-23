b081f089182253b7a14c9ba209a04a7499b54ea3
codeamac01 <codeamac01@gmail.com>
2016-09-23 18:57:36 -0500

    Usuarios pueden registrarse y se guardan exitosamente con password cifrado

--- bitly_example/app/controllers/users_controller.rb
+++ bitly_example/app/controllers/users_controller.rb
@@ -1,22 +1,51 @@
 class UsersController < ApplicationController
-  def index
+  #Index
+  def profile
+    @user = User.find(session[:user_id])
   end
 
-  def create
+  #create
+  def signup
+    valid_user_params = user_params
+    # p"*"*50
+    # p valid_user_params
+    @user = User.new(name: valid_user_params["name"], email: valid_user_params["email"])
+    @user.password=(valid_user_params["password"])
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "El usuario se guardó correctamente"
+      redirect_to profile_path
+    else
+      flash[:error] = "***El usuario no sé guardo correctamente, vuelte a intentarlo***"
+      redirect_to new_user_path
+    end
+  end
+
+
+  def login
+  end
+
+  def logout
   end
 
   def new
+    @user = User.new
   end
 
   def edit
   end
 
-  def show
-  end
+  # def show
+  # end
 
   def update
   end
 
   def destroy
   end
-end
+
+ private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+end#UsersController
--- bitly_example/db/schema.rb
+++ bitly_example/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919232927) do
+ActiveRecord::Schema.define(version: 20160923180346) do
 
   create_table "urls", force: :cascade do |t|
     t.string   "short_url"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20160919232927) do
     t.integer  "visits_count", default: 0
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
