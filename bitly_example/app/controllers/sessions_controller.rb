9c1da0c812424b060cb8875450c587522a422ab0
codeamac01 <codeamac01@gmail.com>
2016-09-26 21:19:52 -0500

    Login y Logout listo :D

--- bitly_example/app/controllers/sessions_controller.rb
+++ bitly_example/app/controllers/sessions_controller.rb
@@ -3,8 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = params[:session]
+    user = User.authenticate(user["email"].downcase, user["password"])
+    if user != nil 
+      session[:user_id] = user.id
+      flash[:success] = ".:: Sesión Iniciada con éxito ::."
+      redirect_to profile_path
+    else
+      flash[:error] = ".:: UPS no se encontró al usuario ::."
+      redirect_to login_path
+    end
   end
 
   def destroy
+    session.clear
+    flash[:success] = ".:: Sesión finalizada ::."
+    redirect_to root_path
   end
 end
