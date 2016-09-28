4962f18e1db9e8c660c285e174ebf34d99bb39a9
codeamac01 <codeamac01@gmail.com>
2016-09-27 19:24:39 -0500

    añadí t.belongs_to :user

--- bitly_example/db/migrate/20160919232927_create_urls.rb
+++ bitly_example/db/migrate/20160919232927_create_urls.rb
@@ -1,6 +1,7 @@
 class CreateUrls < ActiveRecord::Migration
   def change
     create_table :urls do |t|
+      t.belongs_to :user#se crea la llave foránea user_id
       t.string :short_url
       t.string :long_url
       t.integer :visits_count, :default => 0
